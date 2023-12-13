fa6cfb0bcda289b4a6ecceaf5f83ea5fe15f686f
ANAGHARAK <anukulkarni1110@gmail.com>
2023-12-13 19:26:37 +0530

    add new button

--- candidate/views.py
+++ candidate/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages,auth
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.shortcuts import get_object_or_404
+from django.forms import formset_factory
 
 from accounts.models import User
 import candidate
@@ -21,27 +22,50 @@ login_required(login_url='login')
 @user_passes_test(check_role_candidate)
 
 def cprofile(request):
-    
-    if request.method=='POST':
-        form=CandidateForm(request.POST)
-        
-        if form.is_valid():
+    SkillFormSet = formset_factory(CandidateForm, extra=1)
+   
+
+    if request.method == 'POST':
+        skill_formset = SkillFormSet(request.POST, prefix='skills')
+       
+
+        if skill_formset.is_valid():
             cand=Candidate()
             cand.user=request.user
-            cand.skills=form.cleaned_data['skills']
-            cand.certificate_name = form.cleaned_data['certificate_name']
-            cand.issued_by = form.cleaned_data['issued_by']
-           
+            cand.skills=skill_formset.cleaned_data['skills']
+            cand.certificate_name = skill_formset.cleaned_data['certificate_name']
+            cand.issued_by = skill_formset.cleaned_data['issued_by']
             cand.save()
+            # Save the forms and redirect
+            # You need to implement this part based on your application logic
             return redirect('cprofile')
-        else:
-            print(form.errors)
     else:
-        form=CandidateForm()
-    context={
-        'form':form
-    }
-    return render(request,'candidate/cprofile.html',context)
+        skill_formset = SkillFormSet(prefix='skills')
+
+
+    return render(request, 'candidate/cprofile.html', {'skill_formset': skill_formset})
+
+    
+    # if request.method=='POST':
+    #     form=CandidateForm(request.POST)
+        
+    #     if form.is_valid():
+    #         cand=Candidate()
+    #         cand.user=request.user
+    #         cand.skills=form.cleaned_data['skills']
+    #         cand.certificate_name = form.cleaned_data['certificate_name']
+    #         cand.issued_by = form.cleaned_data['issued_by']
+           
+    #         cand.save()
+    #         return redirect('cprofile')
+    #     else:
+    #         print(form.errors)
+    # else:
+    #     form=CandidateForm()
+    # context={
+    #     'form':form
+    # }
+    # return render(request,'candidate/cprofile.html',context)
 
 
 
